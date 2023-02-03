f17a5c70d1d994c0c72fb67ea76e4a704fedda13
Yunwoo Ji <unu12073@gmail.com>
2023-02-04 01:12:39 +0900

    Set up partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import customGithubDarkDimmedTheme from "./custom-github-dark-dimmed.json";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), partytown()],
   markdown: {
     shikiConfig: {
       theme: customGithubDarkDimmedTheme,
