e5209ae2a6f7d6497953a11cf04ca5343c2e70de
Yunwoo Ji <unu12073@gmail.com>
2023-02-04 14:28:21 +0900

    Set up sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
-// https://astro.build/config
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import customGithubDarkDimmedTheme from "./custom-github-dark-dimmed.json";
 import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), partytown()],
+  site: "https://www.unuuuuu.com",
+  integrations: [react(), tailwind(), partytown(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: customGithubDarkDimmedTheme,
